40401080454d614321cf7f78aaf9e7fa665def1f
Ikanant <jonathan.hdez92@gmail.com>
2016-04-05 21:29:20 -0400

    Started using Stylus for compilation to CSS

--- server.js
+++ server.js
@@ -1,12 +1,30 @@
 var express = require('express');
+var stylus = require('stylus');
+var logger = require('morgan');
 
 var env = process.env.NODE_ENV=process.env.NODE_ENV || 'development';
 var port = 3000;
 
 var app = express();
 
+// Compile function used by middleware for using Stylus
+function compile(str, path) {
+    return stylus(str).set('filename', path);
+}
+
 app.set('views', __dirname + '/server/views');
 app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+
+//
+app.use(stylus.middleware(
+    {
+        src: __dirname + '/public',
+        compile: compile
+    }
+));
+app.use(express.static(__dirname + '/public'));
 
 // Match ALL routes (JS, CSS, IMG, anything)
 app.get('*', function(req, res){
